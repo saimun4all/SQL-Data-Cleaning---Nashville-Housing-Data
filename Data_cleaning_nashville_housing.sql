445742dcd44a66306b37b2d665e860fc8308578f
Saimun Hassan <123021229+saimun4all@users.noreply.github.com>
2023-10-22 19:56:25 -0400

    Standardized date format

--- Data_cleaning_nashville_housing.sql
+++ Data_cleaning_nashville_housing.sql
@@ -1,1 +1,22 @@
-SELECT * FROM dbo.[Nashville Housing]
+------------------------------------ DATA CLEANING NASHVILLE EXCEL FILE ---------------------------------
+
+------------------------------------------------------------------------------------------------------
+--- Standardize Date Format
+
+SELECT SaleDateConverted
+FROM PortfolioProject.dbo.[Nashville Housing]
+
+--- Does not Update the Table
+
+UPDATE PortfolioProject.dbo.[Nashville Housing]
+SET SaleDate = CONVERT(Date, SaleDate)
+
+--- Alternate Solution
+
+ALTER TABLE PortfolioProject.dbo.[Nashville Housing]
+Add SaleDateConverted Date;
+
+Update PortfolioProject.dbo.[Nashville Housing]
+SET SaleDateConverted = CONVERT(Date,SaleDate)
+
+
